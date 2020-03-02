1ada041d6a5a6608f58a3b22624f03cbe4945f87
tmoynandy <tmoy.nandy@gmail.com>
2020-03-02 12:42:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# riju-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/riju-react)
---
+++ index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
